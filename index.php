4d45a61ebadbbf439ccf0fbede2f02d1d87cffd5
mariasofia200 <mariarincon1@colegioguanenta.edu.co>
2025-09-02 09:57:21 -0500

    se añadio

--- index.php
+++ index.php
@@ -3,31 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina Feria De Universidades</title>
+    <title>Feria de universidades</title>  
+    <link rel="icon" type="image/x-icon" href="img/logo.jpg">
+    <link rel="stylesheet" href="style.css">
+
 </head>
+<center>
 <body>
-      <h1>Pagina Feria De Universidades</h1>    
-      <h2>Ingresar como:</h2>  
-
-<!--Formulario de inicio de sesion -->
-
-<DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-
-   
+    <div class="container"
+    <header>
+    <div id="encabezado-principal"><h1>Feria de universidades</h1></div>
+    <img src="img/logo.jpg"alt="feria" width="500" height="350">
     
-    <hr>
-    <a href="loguearse_adm.php">Administrador</a>
-    <hr>
-    <a href="registrar_departamento.php">Delegado</a>
-    <hr>
-    <a href="registrar_apoyo.php">Apoyo</a>
-    <hr>
-    <a href="registrar_guia.php">Guia</a>
 
+</header>
+<h3><em>La XXI Feria de Universidades del Colegio San José de Guanentá, realizada en San Gil los días 25 y 26 de julio de 2019, es un evento académico orientado a los estudiantes de grado 11 de la región Guanentina y Comunera. Su propósito es brindar orientación vocacional y profesional, mediante la participación de instituciones de educación superior, media y básica, públicas y privadas, que presentan sus programas académicos de nivel superior, técnico y tecnológico en un espacio de socialización e integración.</em></h3>
+<button><a href="iniciar_sesion.php">iniciar sesion</a></button>
+
+<footer>
+        &COPY; 2025
+    </footer>
 </body>
+</center>
 </html>
---
+++ iniciar_sesion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Feria De Universidades</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<center>
+<body>
+      <h1>Pagina Feria De Universidades</h1>    
+      <h2>Ingresar como:</h2>  
+
+<!--Formulario de inicio de sesion -->
+
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <hr>
+    <button><a href="loguearse_adm.php">Administrador</a></button>
+    
+    <button><a href="registrar_departamento.php">Delegado</a></button>
+    
+    <button><a href="registrar_apoyo.php">Apoyo</a></button>
+    
+    <button><a href="registrar_guia.php">Guia</a></button>
+</body>
+</center>
+</html>
